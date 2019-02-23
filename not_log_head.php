d48c87f409c69183e76e534884d4ec803138fbb8
lzxaini <35051474+lzxaini@users.noreply.github.com>
2019-02-23 14:11:15 +0800

    Update not_log_head.php

--- not_log_head.php
+++ not_log_head.php
@@ -42,7 +42,7 @@
 					    <a href="admin/logoin.php"><span class="glyphicon glyphicon-piggy-bank"></span> 管理员登录</a>
 					</li>
 				   <li>
-				   	<a href="关于.php"><span class="glyphicon glyphicon-tree-deciduous"></span> 简介</a>
+				   	<a href="About.php"><span class="glyphicon glyphicon-tree-deciduous"></span> 简介</a>
 				   </li>
 <!-- 					<li><a href="#"><span class="glyphicon glyphicon-question-sign"></span>&nbsp;关于</a></li> -->
 				</ul>				
